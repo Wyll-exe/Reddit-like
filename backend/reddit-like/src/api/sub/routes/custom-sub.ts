2c355a01f93449d96c70fa0d8126694bf42f6f3c
Zaco <cyrilmathe08@gmail.com>
2025-05-06 10:20:46 +0200

    :sparkles: ajout d'une route pour supprimer et d'une pour modifier

---
+++ backend/reddit-like/src/api/sub/routes/custom-sub.ts
@@ -0,0 +1,22 @@
+import { config } from "process";
+
+export default {
+    routes : [
+        {
+            method: 'PUT',
+            path: '/subs/:id',
+            handler: 'api::sub.sub.modify',
+            config: {
+                auth: false,
+            }
+        },
+        {
+            method: 'DELETE',
+            path: 'subs/:id',
+            handler: 'api::sub.sub.delete',
+            config: {
+                auth: false,
+            }
+        }
+    ]
+}
