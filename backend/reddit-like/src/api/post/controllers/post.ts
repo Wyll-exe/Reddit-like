ccef2bdaf19e7e05bcc428b66294e4c24a76af62
Zaco <cyrilmathe08@gmail.com>
2025-05-06 10:22:13 +0200

    :fire:

--- backend/reddit-like/src/api/post/controllers/post.ts
+++ backend/reddit-like/src/api/post/controllers/post.ts
@@ -6,14 +6,4 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::post.post', () => ({
-    async modify(ctx) {
-        try {
-            ctx.body = 'ok hello';
-            console.log("aa");
-        } catch (err) {
-            ctx.body = err;
-            console.log("zz");
-        }
-    }
-}));
+export default factories.createCoreController('api::post.post');
