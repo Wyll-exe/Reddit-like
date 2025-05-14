35c54e9e58defbfa86f843a8453189ecfc03efd6
Zaco <cyrilmathe08@gmail.com>
2025-05-14 21:45:44 +0200

    :wastebasket: clean

--- backend/reddit-like/src/api/sub/routes/sub.ts
+++
@@ -1,7 +0,0 @@
-/**
- * sub router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::sub.sub');
--- backend/reddit-like/src/extensions/users-permissions/controllers/utils/index.js
+++
@@ -1,9 +0,0 @@
-        // ../users-permissions/utils/index.js
-    
-        'use strict';
-        const getService = name => {
-          return strapi.plugin('users-permissions').service(name);
-        };
-        module.exports = {
-          getService,
-        };
