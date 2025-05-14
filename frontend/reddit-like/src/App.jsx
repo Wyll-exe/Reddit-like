54e6675f9cc6e4fabb501aefb869c0225e75d25c
Zaco <cyrilmathe08@gmail.com>
2025-05-14 02:15:40 +0200

    Revert "Merge branch 'feature/user' into dev"
    This reverts commit da14c8d56ab14d2acf8d1c5630b7fa90fd30c4c8, reversing
    changes made to a27bc0877fdf667086eb5863a4dc91eabcec391e.

--- frontend/reddit-like/src/App.jsx
+++ frontend/reddit-like/src/App.jsx
@@ -13,7 +13,6 @@ import ModifierPost from "./components/Posts/ModifierPost.jsx";
 import DeletePost from "./components/Posts/DeletePost.jsx";
 import PostDetails from "./components/Posts/PostDetails.jsx";
 import NotFound from "./pages/404NotFound.jsx";
-import Profile from "./components/User/Profile.jsx";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -34,7 +33,6 @@ function App() {
           <Route path="/homepage/:id" element={<ModifierPost />} />
           <Route path="/homepage/supp/:documentId" element={<DeletePost />} />
           <Route path="/post/:documentId" element={<PostDetails />} />
-          <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
     </Router>
